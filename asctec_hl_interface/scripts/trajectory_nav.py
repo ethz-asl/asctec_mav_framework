066d2949e5f80f2ee1c9ba12354ee422780c6dd4
Markus Achtelik <markus.achtelik@mavt.ethz.ch>
2012-02-25 20:59:57 +0100

    trajectory nav worink and tested with relative and absolute wp flying

--- asctec_hl_interface/scripts/trajectory_nav.py
+++ asctec_hl_interface/scripts/trajectory_nav.py
@@ -39,16 +39,38 @@ import actionlib
 import re
 import time
 import sys
+import argparse
+import math
 
 from asctec_hl_comm.msg import mav_ctrl
+from geometry_msgs.msg import PoseStamped
 
 global seq
+global have_pose
+global start_pose
 
-def parseWaypoints():
+class PoseXYZYaw:
+    x = 0
+    y = 0
+    z = 0
+    yaw = 0
+    c_yaw = 1
+    s_yaw = 0
+    
+    def __init__(self, pose):
+        self.x = pose.pose.position.x
+        self.y = pose.pose.position.y
+        self.z = pose.pose.position.z
+        q = pose.pose.orientation
+        self.yaw = math.atan2(2.0 * (q.w * q.z + q.x * q.y), 1.0 - 2.0 * (q.y * q.y + q.z * q.z))
+        self.c_yaw = math.cos(self.yaw)
+        self.s_yaw = math.sin(self.yaw)
+
+def parseWaypoints(wp_file):
     rospy.loginfo("parsing waypoint list...")
     # read file
     try:
-        text = open(sys.argv[1],"r").readlines()
+        text = open(wp_file,"r").readlines()
     except:
         print 'unable to open file %s'%sys.argv[1]
         return False
@@ -78,7 +100,7 @@ def parseWaypoints():
     rospy.loginfo("successfully parsed " + str(len(waypoints)) + " waypoints")
     return waypoints
 
-def sendWaypoints(wp, pub):
+def sendWaypoints(wp, pub, sp):
     global seq
     goal = mav_ctrl()
 
@@ -97,14 +119,27 @@ def sendWaypoints(wp, pub):
     wps = wp.split();
     
     try:
-        goal.x = float(wps[0])
-        goal.y = float(wps[1])
-        goal.z = float(wps[2])
-        goal.yaw = float(wps[3])
+        x = float(wps[0])
+        y = float(wps[1])
+        z = float(wps[2])
+        yaw = float(wps[3])
         goal.v_max_xy = float(wps[4])
         goal.v_max_z = float(wps[5])
     except: 
         pass
+
+    goal.x = sp.c_yaw * x - sp.s_yaw * y + sp.x
+    goal.y = sp.s_yaw * x + sp.c_yaw * y + sp.y
+    goal.z = z + sp.z
+    goal.yaw = yaw + sp.yaw
+    
+    if(goal.yaw > math.pi):
+        goal.yaw = goal.yaw - 2*math.pi
+    
+    if(goal.yaw < -math.pi):
+        goal.yaw = goal.yaw + 2*math.pi
+        
+        
     
 #    rospy.loginfo("sending waypoint: " + "\t" + str(goal.x) + "\t" + str(goal.y) + "\t" + str(goal.z))
     pub.publish(goal)
@@ -114,20 +149,59 @@ def sendWaypoints(wp, pub):
     except:
         pass
 
+def poseCallback(msg):
+    global have_pose    
+    global start_pose
+    
+    start_pose = msg
+    have_pose = True
 
 if __name__ == '__main__':
     try:
 
         seq = 0
         
-        if(len(sys.argv)==3):
-            pub = rospy.Publisher(sys.argv[2] + '/fcu/control', mav_ctrl)
-        else:
-            pub = rospy.Publisher('fcu/control', mav_ctrl)
-        
         rospy.init_node('wpclient')
+#        start_pose = PoseStamped()
+#        start_pose.pose.position.x = 0
+#        start_pose.pose.position.y = 0
+#        start_pose.pose.position.z = 0
+#        start_pose.pose.orientation.w = 1
+#        start_pose.pose.orientation.x = 0
+#        start_pose.pose.orientation.y = 0
+#        start_pose.pose.orientation.z = 0
         
-        wp_list = parseWaypoints();
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-f', '--file', help='trajectory file', required=True)
+        parser.add_argument('-m', '--mode', default='abs', help='define if absolute (abs) trajectory coordinates are used or relative (rel) to the current position')
+        
+        args = parser.parse_args(rospy.myargv()[1:]);
+        
+        if args.mode == 'abs':
+            have_pose = True
+            print 'operating in absolute mode'
+        elif args.mode == 'rel':
+            sub = rospy.Subscriber('fcu/current_pose', PoseStamped, poseCallback)
+            print 'operating in relative mode'
+            have_pose = False
+        else:
+            rospy.logerr('Unknown mode')
+            exit()
+        
+        n_wait = 0;
+        while not have_pose:
+            time.sleep(0.1)
+            n_wait = n_wait + 1;
+            if n_wait > 20:
+                rospy.logerr('didn\'t receive start pose')
+                exit()
+  
+        simple_start_pose = PoseXYZYaw(start_pose)        
+        print 'starting from x=%f y=%f z=%f yaw=%f deg\n' % (simple_start_pose.x, simple_start_pose.y, simple_start_pose.z, simple_start_pose.yaw*180/math.pi)    
+                
+        pub = rospy.Publisher('fcu/control', mav_ctrl)        
+         
+        wp_list = parseWaypoints(args.file);
         
         if not (wp_list):
             exit()
@@ -141,7 +215,7 @@ if __name__ == '__main__':
         for line in wp_list:
             if rospy.is_shutdown():
                 break
-            sendWaypoints(line, pub)
+            sendWaypoints(line, pub, simple_start_pose)
     except rospy.ROSInterruptException:
         print "program interrupted before completion"
 
