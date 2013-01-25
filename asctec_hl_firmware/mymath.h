2d0619010a5d79c870326487dfce810427317b00
Markus Achtelik <markus.achtelik@mavt.ethz.ch>
2013-01-25 19:04:16 +0100

    added approximated cosine function

--- asctec_hl_firmware/mymath.h
+++ asctec_hl_firmware/mymath.h
@@ -26,6 +26,9 @@ DAMAGE.
 
  */
 
+#ifndef MYMATH
+#define MYMATH
+
 #define sq(x) (x*x);
 #define M_PI 3.1415926535897932384626433832795
 #define M_2PI 6.283185307179586476925286766559
@@ -33,4 +36,8 @@ DAMAGE.
 #define M_halfPI 1.5707963267948966192313216916398
 
 
+float approxCos(float x);
 
+
+
+#endif
