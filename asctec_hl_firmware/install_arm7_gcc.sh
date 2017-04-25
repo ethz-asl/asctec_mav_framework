b08f02896d0f4358215d50ed70b2cf4552beec33
fmina <fmina@ethz.ch>
2017-04-26 00:17:00 +0200

    install arm7-gcc in tmp folder

--- asctec_hl_firmware/install_arm7_gcc.sh
+++ asctec_hl_firmware/install_arm7_gcc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-rm -rf /tmp/arm7-gcc-2012-03-28-bin
-mkdir /tmp/arm7-gcc-2012-03-28-bin
-cd /tmp/arm7-gcc-2012-03-28-bin
+rm -rf /tmp/arm7-elf-gcc
+mkdir /tmp/arm7-elf-gcc
+cd /tmp/arm7-elf-gcc
 curl -S http://www.asctec.de/downloads/software/sdk/arm7-gcc-2012-03-28-bin.tar.bz2 > arm7-gcc-2012-03-28-bin.tar.bz2
 tar -xvf arm7-gcc-2012-03-28-bin.tar.bz2
 
@@ -10,14 +10,13 @@ sudo apt-get install -y libx11-6 libx11-dev libncurses5-dev libncursesw5-dbg lib
 
 sudo apt-get install -y lib32z1 lib32bz2-1.0
 
-export PATH=$PATH:/usr/local/arm7/bin
-sudo tar -C / -xjf arm7-gcc-2012-03-28.tar.bz2 
+tar -xvf arm7-gcc-2012-03-28.tar.bz2
 
-cd /usr/local/arm7/bin
+cd usr/local/arm7/bin
 
 for i in * 
 do
    sudo chmod a+x $i
 done
 
-
+#export PATH=$PATH:/tmp/arm7-elf-gcc/usr/local/arm7/bin
